6480fc066306b2536c8e66f7f49a914d501e33d9
Klejdi Vrekaj <vrekaj.1@osu.edu>
2022-04-15 18:28:17 -0400

    Added some options to venadmin and paradmin

--- paradmin.php
+++ paradmin.php
@@ -10,7 +10,18 @@
    </head>
    
    <body style = "background-color:lightblue; text-align:center">
-      <h1>Welcome Parking Administrator</h1> 
+      <h1>Welcome, Parking Administrator</h1>
+      <br>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="add_garage.php">Add a garage</a></h3>
+      </div>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="update_garage.php">Update a garage</a></h3>
+      </div>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="update_price.php">Update price</a></h3>
+      </div>
+      <br>
       <h2><a href = "log_out.php">Log Out</a></h2>
    </body>
    
--- venadmin.php
+++ venadmin.php
@@ -10,7 +10,18 @@
    </head>
    
    <body style = "background-color:lightblue; text-align:center">
-      <h1>Welcome Venue Administrator</h1> 
+      <h1>Welcome, Venue Administrator</h1>
+      <br>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="add_venue.php">Add a venue</a></h3>
+      </div>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="update_venue.php">Update a venue</a></h3>
+      </div>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h3><a href="add_event.php">Add an event</a></h3>
+      </div>
+      <br>
       <h2><a href = "log_out.php">Log Out</a></h2>
    </body>
    
