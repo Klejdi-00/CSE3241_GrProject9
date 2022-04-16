387e4488ded45d3b178616ed0ab0c1b488113102
Klejdi Vrekaj <vrekaj.1@osu.edu>
2022-04-16 13:24:04 -0400

    Stylistic changes

--- paradmin.php
+++ paradmin.php
@@ -13,16 +13,20 @@
       <h1>Welcome, Parking Administrator</h1>
       <br>
       <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <br>
          <h3><a href="add_garage.php">Add a garage</a></h3>
-      </div>
-      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <br>
+         <br>
          <h3><a href="update_garage.php">Update a garage</a></h3>
-      </div>
-      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <br>
+         <br>
          <h3><a href="delete_garage.php">Delete a garage</a></h3>
+         <br>
       </div>
       <br>
-      <h2><a href = "log_out.php">Log Out</a></h2>
+      <div class='col-md-3' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px" >
+         <h2 style="padding:10px"><a href = "log_out.php">Log Out</a></h2>
+      </div>
    </body>
    
 </html>
