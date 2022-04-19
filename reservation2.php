b271177b2b77a074c5238534caeca0324ddc3dc2
Klejdi Vrekaj <vrekaj.1@osu.edu>
2022-04-18 23:31:04 -0400

    Added initial reservation files

---
+++ reservation2.php
@@ -0,0 +1,56 @@
+<?php 
+include("session.php");
+
+// Register user
+if(isset($_POST['btnevent'])){
+    // Getting variable from input
+    $event = $_GET['event'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Reservation | ParkingMaster</title>
+    <!-- Bootstrap CSS Stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  </head>
+  <body style = "background-color:lightblue; text-align:center">
+    <h1>ParkingMaster</h1>
+    <br>
+    <div class='col-md-4' style="float:none; margin:auto; background-color:aliceblue; border-radius:25px">
+          <form method='post' action=''>
+            <h2 style="padding-top:25px">Reservation</h2>
+            <br>
+            <div class="form-group">
+                <label for="event">Select an Event:</label>
+                <select class="form-control" style="text-align:center" name="event" required>
+                    <option selected value="">Choose an Event</option>
+                    <?php
+                    // Getting all the venues in an array
+                    $events = array();
+                    $start = array();
+                    $end = array();
+                    $venues = array();
+                    $sql = "select * from event_list";
+                    $qrry = $connect->query($sql);
+                    while($row = $qrry->fetch_assoc()) {
+                        $events[] = $row["event_name"];
+                        $start[] = $row["start_date"];
+                        $end[] = $row["end_date"];
+                        $venues[] = $row["venue"];
+                    }
+                    for($i = 0; $i < sizeof($events); $i++) {
+                        echo '<option value="'.$events[$i].'">'. $events[$i]. ' at ' .$venues[$i].' ('.$start[$i].' to '.$end[$i].')</option>';
+                    }
+                    ?>
+                </select>
+                <h3 style="padding:10px"><a href = "reservation2.php?event=<?php echo $event ?>">Continue</a></h3>
+            </div>
+          </form>
+          <br>
+    </div>
+    <br>
+    <h3>Return to <a href="customer.php"> Homepage</a></h3>
+  </body>
+</html>
